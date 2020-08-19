6d9579444e0be5f5daece11ececaf000d69f51b0
contact@danielkorgel.com <264778848eca87c3d106916245f7c66c>
2020-08-19 23:24:29 +0200

    implemented eye behavior and eye rendering

--- Cyborg-1.5-Oled-Eye.ino
+++ Cyborg-1.5-Oled-Eye.ino
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
-#include "Eye-Behavior/Eye.h"
+#include <Eye.h>
+
 using namespace EyeBehavior;
 
 #ifdef U8X8_HAVE_HW_SPI
@@ -16,15 +17,30 @@ U8G2_SSD1327_WS_128X128_1_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 10, /* dc=*/ 9, /* re
 
 
 EyeConfig eyeconfig;
- Eye eyeValue(&eyeconfig);
+Eye eye(&eyeconfig);
 
   
 void setup() {
 
- 
   // put your setup code here, to run once:
   Serial.begin(9600);
 
+  eyeconfig.blink_timing_duration.min = 65;
+  eyeconfig.blink_timing_duration.max = 85;
+
+  eyeconfig.blink_timing_in_between.min = 1000;
+  eyeconfig.blink_timing_in_between.max = 7000;
+
+  eyeconfig.eyelid_upper_y_range.min = 0.1;
+  eyeconfig.eyelid_upper_y_range.max = 0.5;    
+  eyeconfig.eyelid_lower_y_range.min = 0.9;
+  eyeconfig.eyelid_lower_y_range.max = 0.5; 
+
+  eyeconfig.eyelid_movement_speed = 0.5;
+
+  eyeconfig.pupil_size_speed = 0.2;
+  
+  eye.init_pos();
  
   /* U8g2 Project: SSD1306 Test Board */
   pinMode(10, OUTPUT);
@@ -44,7 +60,6 @@ void u8g2_prepare(void) {
   u8g2.setFontDirection(0);
 }
 
-int i=0;
 int posX = 64;
 int posY = 64;
 
@@ -53,36 +68,57 @@ int move = 1;
 
 void loop() {
 
-u8g2.firstPage();  
+  eye.update(millis());
+  posX = 64 + (eye.get_position_x()*20);
+  posY = 64 + (eye.get_position_y()*10);
+
+#if DEBUG_POS
+  Serial.print("Values: ");
+  Serial.print(posX);
+  Serial.print(" - ");
+  Serial.println(posY);
+#endif
+
+  /*Serial.print("Eyelid: ");
+  Serial.print(lower_eyelid);
+  Serial.print(" - ");
+  Serial.println(eye.get_upper_eyelid());*/
+
+  //Serial.print("Pupil: ");
+  //Serial.println(eye.get_pupil_size());
+
+  // u8g2.clearBuffer();
+  u8g2.firstPage();  
   do {
-       u8g2_prepare();
+      u8g2_prepare();
   // u8g2.drawStr( 0, 0, "Hello World");
   //u8g2.drawStr( 0, 8, "Daniel Korgel");
  
   //u8g2.drawStr( 0, 3, i);
-
+  
+  //eye ball
   u8g2.drawDisc(posX,posY,48);
   u8g2.setDrawColor(0);
-  u8g2.drawDisc(posX,posY,16);
-  for(int i=1; i<8; i++){
-    u8g2.drawCircle(posX,posY,16+(i*2));
-  }
-  } while( u8g2.nextPage() );
-
-  if(move > 0 && posX < 78){
-    posX += 2;
-    if(posX >= 78){
-      move = -1;
-    }
-  }
-  if(move < 0 && posX > 50){
-    posX -= 2;
-    if(posX <= 50){
-      move = 1;
-    }
-  }
+  u8g2.drawCircle(posX,posY,48);
+  u8g2.drawCircle(posX,posY,48-1.5);
+  //u8g2.drawCircle(posX,posY,48-2);
   
-  //Serial.println("Test");
-  //delay(50);
-  i+=1;
+  // pupil
+  u8g2.setDrawColor(0);
+  u8g2.drawDisc(posX,posY,20*eye.get_pupil_size());
+
+
+  // iris
+  for(int i=1; i<12; i++){
+    u8g2.drawCircle(posX,posY,8+(i*2));
+  }
+  // Eyelids
+  u8g2.setDrawColor(0);
+  u8g2.drawBox(posX-48, posY-48,98,96*eye.get_upper_eyelid());
+  float lower_eyelid = eye.get_lower_eyelid();
+  u8g2.drawBox(posX-48, posY-48+(98*(lower_eyelid)),98,48);
+  } while( u8g2.nextPage() );
+  //u8g2.sendBuffer();
+  
+  //delay(5);
 }
